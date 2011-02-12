bd7e3f39272792fb7d2dd571680b20b29fbb7336
Chris Conover <clcono@gmail.com>
2011-02-12 13:18:17 -0500

    Resolved back scan logic issues

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import sys, os, re, types, os.path
+import sys, os, re, types, os.path, time, logging, logging.handlers
 from datetime import datetime
 
 # Special Cases
@@ -9,7 +9,7 @@ from datetime import datetime
 ## Precise log does not exist but range
 
 FILE_SIZE 	= 0
-TODAY 		= datetime.today()
+TODAY 		= datetime(2011, 2, 11)
 MONTH_MAP	= {	'jan':1,
 				'feb':2,
 				'mar':3,
@@ -26,62 +26,116 @@ MONTH_MAP	= {	'jan':1,
 
 class RedditLogFile(object):
 	
+	class SearchingError(Exception):
+		'''An error occurred when trying to search.'''
+		pass
+	
 	def __init__(self, filename):
 		self.file = open(filename, 'rb', 0)
 		self.file_size = os.path.getsize(filename)
-		
+	
 	def search(self, start_ts, end_ts):
-		
-		duration = False if end is None else True
+		'''
+			Searches for logs matching a specfic timestamp or for those that
+			fall into a range of timestamps
+			
+			Algorithm:
+			-> Compare the specified starting timestamp to the current line
+			- If it's greater, we passed our desired log and we move the 
+			file cursor back half way to the fence.
+			- If it's less, we haven't reached it yet and we move the 
+			file cursor foward half way to the fence
+			- If it matches, scan backward to make sure we have the first log
+			in a set of potentially manner logs with the same timestamp
+			- Do the same as above for the end timestamp, just reversed
+		'''
+		duration = False if end_ts is None else True
 		
 		# Check some special cases
-		if start >= end_ts:
-			raise ValueError, 'Starting timestamp must be less than ending timestamp' 
 		last_offset, last_ts = self._date_at_offset(self.file_size - 1)
 		if start_ts > last_ts:
 			# Last log's timestamp is less than specified start
-			return ()
+			raise self.SearchingError, 'Starting timestamp is beyond last log'
 		if duration:
+			if start_ts >= end_ts:
+				raise self.SearchingError, 'Starting timestamp must be less than ending timestamp'
 			first_offset, first_ts = self._date_at_offset(0)
 			if end_ts < first_ts:
 				# First log's timestamp is greater than specified end
-				return ()
+				raise self.SearchingError, 'Ending timestamp is before first log'
 
 		start_offset, end_offset = None, None
 		
 		# Find the start log offset
-		self.file.fseek(0)
+		self.file.seek(0)
 		fence = self.file_size
+		upper_bound, lower_bound = None, None
 		while True:
 			current_offset, current_ts = self._date_at_offset()
 
-			fence = int(fence / 2)
-			#print 'Bounding: ' + str(bounding) + ' ' + str(start_ts) + ' ' + str(line_ts)
-			if start_ts > current_ts: # Passed it	
-				file.seek(current_offset + fence)
-			elif start_ts < current_ts: # Haven't reached it yet
-				file.seek(current_offset - fence)
+			fence = fence / 2
+			print 'Fence: ' + str(fence) + ' ' + str(start_ts) + ' ' + str(current_ts)
+			if current_ts < start_ts: # Not there yet. Jump foward
+				self.file.seek(current_offset + fence)
+				lower_bound = current_offset
+			elif current_ts > start_ts: # Passed it. Jump back
+				self.file.seek(current_offset - fence)
+				upper_bound = current_offset
 			elif start_ts == current_ts:
-				# Scan backward to make sure we have the
-				# first log of this timestamp
-				while current_offset > 0:
-					prev_offset, prev_ts = self._date_at_offset(current_offset - 1)
-					if current_ts != prev_ts:
-						break
+				# Scan backward to find the first of potentially many
+				# logs with the same timestamp
+				
+				# Lower bound is no good if the last fence jump
+				#lower_bound = 0
+				scan_lower = lower_bound
+				scan_upper = current_offset
+				while True:
+					#time.sleep(2)
+					look_offset, look_ts = self._date_at_offset()
+					print ' ' + str(look_ts) + ' ' + str(current_ts)
+					if look_ts == current_ts:
+						check_offset, check_ts = self._date_at_offset(look_offset - 1)
+						print ' ' + str(check_ts)
+						if check_ts != current_ts:
+							# Found the border
+							break
+						else:
+							scan_upper = look_offset
+							print 'Previous still matches, go back farther %d' % ((look_offset - scan_lower) / 2)
+							# Not far enough
+							self.file.seek(look_offset - ((look_offset - scan_lower) / 2))
+							
 					else:
-						current_offset = prev_offset
-						current_ts = prev_ts
+						self.file.readline() # Adavanced to next log
+						check_offset, check_ts = self._date_at_offset()
+						print ' ' + str(check_ts)
+						if check_ts == current_ts:
+							# Found the border
+							break
+						else:
+							scan_lower = look_offset
+							print 'Next does not match too far, go foward %d' % ((scan_upper - look_offset) / 2)
+							# Went back too far
+							self.file.seek(look_offset + ((scan_upper - look_offset) / 2))
 				break
 			elif fence == 1 and duration:
 				# Closest we are going to get for range
 				break
 			elif fence == 1:
 				# No precise match as requested
-				return ()
+				return
 				
 		start_offset = self.file.tell()
+		#print self._date_at_offset()
 		
-		if duration:
+		self.file.seek(start_offset - 100)
+		for i in range(1, 10):
+			print self._date_at_offset()
+		self.file.close()
+		return
+		if not duration:
+			end_offset = last_offset
+		else:
 			# Find the end log offset
 			fence = self.file_size - start_log_offset # End log must be past start log
 			while True:
@@ -110,10 +164,11 @@ class RedditLogFile(object):
 					end_offset = self.file_size
 					break
 					
-		self.file.fseek(start_offset)
+		self.file.seek(start_offset)
 		while end_offset > self.file.tell():
 			yield self.file.readline()
-				
+		self.file.close()
+		
 	def _date_at_offset(self, offset = None):
 		'''
 			Backtracks to find the beginning of the current line.
@@ -136,7 +191,7 @@ class RedditLogFile(object):
 			reads += 1
 		return self.file.tell(), self._date_from_line(self.file.readline())
 		
-	def _date_from_line(line):
+	def _date_from_line(self, line):
 		'''
 			Parse date from line. Expects Reddit log format.
 			Returns datetime
@@ -207,5 +262,8 @@ if __name__ == '__main__':
 			except IOError:
 				print 'Unable to open log file'
 			else:
-				for log in reddit_logs.search(start_ts, end_ts):
-					print log
+				try:
+					for log in reddit_logs.search(start_ts, end_ts):
+						print log
+				except RedditLogFile.SearchingError, e:
+					print 'Error: ' + str(e)
